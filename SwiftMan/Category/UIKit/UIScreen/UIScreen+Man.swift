368a7ec6424c0178970f5d8d96d1eb90e8a99939
cactus <522941939@qq.com>
2016-04-29 14:07:17 +0800

    add UIScreen+Man.swift

---
+++ SwiftMan/Category/UIKit/UIScreen/UIScreen+Man.swift
@@ -0,0 +1,28 @@
+//
+//  UIScreen+Man.swift
+//  SwiftMan
+//
+//  Created by neu on 16/4/29.
+//  Copyright © 2016年 cactus. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIScreen {
+
+    
+    public var m_mainScreenLongSide: CGFloat {
+        let  screenSize = UIScreen.mainScreen().bounds.size
+        return screenSize.width > screenSize.height ? screenSize.width : screenSize.height;
+    }
+    
+    public var m_mainScreenShortSide: CGFloat {
+        let  screenSize = UIScreen.mainScreen().bounds.size
+        return screenSize.width > screenSize.height ? screenSize.height : screenSize.width;
+
+    }
+    
+    
+    
+}
