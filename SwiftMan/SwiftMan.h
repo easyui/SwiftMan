c0ba94ccc330f067b92870a0874f597e39c1b9a9
cactus <522941939@qq.com>
2016-04-29 11:04:08 +0800

    into framework

---
+++ SwiftMan/SwiftMan.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMan.h
+//  SwiftMan
+//
+//  Created by neu on 16/4/29.
+//  Copyright © 2016年 cactus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMan.
+FOUNDATION_EXPORT double SwiftManVersionNumber;
+
+//! Project version string for SwiftMan.
+FOUNDATION_EXPORT const unsigned char SwiftManVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMan/PublicHeader.h>
+
+
