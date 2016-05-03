8502dd22d1783d49ba1968401d2481c502f77f7f
cactus <522941939@qq.com>
2016-05-03 22:29:38 +0800

    add CGRect+Man.swift

---
+++ SwiftMan/Extension/CoreGraphics/CGRect/CGRect+Man.swift
@@ -0,0 +1,52 @@
+//
+//  CGRect+Man.swift
+//  SwiftMan
+//
+//  Created by neu on 16/5/3.
+//  Copyright © 2016年 cactus. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+extension CGRect {
+    
+    // X value of CGRect's origin
+    public var x: CGFloat {
+        get {
+            return self.origin.x
+        } set(value) {
+            self.origin.x = value
+        }
+    }
+    
+    // Y value of CGRect's origin
+    public var y: CGFloat {
+        get {
+            return self.origin.y
+        } set(value) {
+            self.origin.y = value
+        }
+    }
+    
+    // Width of CGRect's size
+    public var w: CGFloat {
+        get {
+            return self.size.width
+        } set(value) {
+            self.size.width = value
+        }
+    }
+    
+    // Height of CGRect's size
+    public var h: CGFloat {
+        get {
+            return self.size.height
+        } set(value) {
+            self.size.height = value
+        }
+    }
+    
+}
