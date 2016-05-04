fda0c92d5f010fbf87e53d48141795554ddf8e9f
cactus <522941939@qq.com>
2016-05-04 11:07:13 +0800

    add    public static func m_executeOnMainQueueAfterTimeInterval(seconds: NSTimeInterval,block: dispatch_block_t) {                  m_executeAfterTimeInterval(seconds, queue: dispatch_get_main_queue(), block: block)     }          public static func m_executeAfterTimeInterval(seconds: NSTimeInterval, queue: dispatch_queue_t, block: dispatch_block_t) {         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))         dispatch_after(time, queue, block)     }

--- SwiftMan/Extension/Foundation/NSTimer/NSTimer+Block.swift
+++ SwiftMan/Extension/Foundation/NSTimer/NSTimer+Block.swift
@@ -21,5 +21,14 @@ extension NSTimer {
         let block: dispatch_block_t = unsafeBitCast(timer.userInfo, dispatch_block_t.self) as dispatch_block_t;
         block()
     }
-       
+    
+    public static func m_executeOnMainQueueAfterTimeInterval(seconds: NSTimeInterval,block: dispatch_block_t) {
+        
+        m_executeAfterTimeInterval(seconds, queue: dispatch_get_main_queue(), block: block)
+    }
+    
+    public static func m_executeAfterTimeInterval(seconds: NSTimeInterval, queue: dispatch_queue_t, block: dispatch_block_t) {
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+        dispatch_after(time, queue, block)
+    }
 }
