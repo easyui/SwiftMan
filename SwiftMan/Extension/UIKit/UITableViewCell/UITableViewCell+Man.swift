f41119dd72c600edffa77d6ac8a472762f398465
cactus <522941939@qq.com>
2016-06-22 06:42:17 +0800

    add UITableViewCell+Man.swift

---
+++ SwiftMan/Extension/UIKit/UITableViewCell/UITableViewCell+Man.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell+Man.swift
+//  SwiftMan
+//
+//  Created by neu on 16/6/22.
+//  Copyright © 2016年 cactus. All rights reserved.
+//
+
+import UIKit
+
+
+extension UITableViewCell{
+    public static func nib() -> UINib {
+        return  UINib(nibName: String(self), bundle: nil)
+    }
+}
