4a0ef9f1ed2a93401e249f18d7260614030ae45a
easyui <gzhuyangjun@gmail.com>
2017-01-11 14:09:45 +0800

    UIColor+Man.swift

--- SwiftMan/Extension/UIKit/UIColor/UIColor+Man.swift
+++ SwiftMan/Extension/UIKit/UIColor/UIColor+Man.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 extension UIColor {
-    // init method with RGB values from 0 to 255, instead of 0 to 1. With alpha(default:1)
+    
+    /// init
+    ///
+    /// - Parameters:
+    ///   - r: RGB values from 0 to 255, instead of 0 to 1.
+    ///   - g: RGB values from 0 to 255, instead of 0 to 1.
+    ///   - b: RGB values from 0 to 255, instead of 0 to 1.
+    ///   - a: alpha(default:1)
     public convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1) {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: a)
     }
     
-    // init method with hex string and alpha(default: 1) ex: "0x111111","#111111"
+    /// init?
+    ///
+    /// - Parameters:
+    ///   - hexString: hex string, ex: "0x111111","#111111"
+    ///   - alpha: alpha(default:1)
     public convenience init?(hexString: String, alpha: CGFloat = 1.0) {
         var formatted = hexString.replacingOccurrences(of: "0x", with: "")
         formatted = formatted.replacingOccurrences(of: "#", with: "")
@@ -25,7 +36,11 @@ extension UIColor {
         }
     }
     
-    // init method with hex int and alpha(default: 1)
+    /// init
+    ///
+    /// - Parameters:
+    ///   - rgbHex: hex int
+    ///   - alpha: alpha(default: 1)
     public convenience init(rgbHex: Int, alpha: CGFloat = 1) {
         let red = (rgbHex >> 16) & 0xFF
         let green = (rgbHex >> 8) & 0xFF
@@ -34,6 +49,11 @@ extension UIColor {
     }
     
     //  Returns random UIColor with random alpha(default: false)
+    
+    /// 随机颜色
+    ///
+    /// - Parameter isRandomAlpha: 透明度随机
+    /// - Returns: 返回随机颜色
     public static func m_randomColor(_ isRandomAlpha: Bool = false) -> UIColor {
         func random() -> CGFloat {
             let upper = 255;
@@ -46,7 +66,8 @@ extension UIColor {
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: alpha)
     }
     
-    // 0x66ccff
+    
+    /// ex: 0x66ccff
     public var m_RGBHex: Int{
         var r: CGFloat = 0 ,g: CGFloat = 0,b: CGFloat = 0
         getRed(&r, green: &g, blue: &b, alpha: nil)
@@ -56,7 +77,7 @@ extension UIColor {
         return (red << 16) + (green << 8) + blue;
     }
     
-    //0x66ccffff
+    /// ex: 0x66ccffff
     public var m_RGBAHex: Int{
         var r: CGFloat = 0 ,g: CGFloat = 0,b: CGFloat = 0,a: CGFloat = 0
         getRed(&r, green: &g, blue: &b, alpha: &a)
@@ -67,28 +88,28 @@ extension UIColor {
         return (red << 24) + (green << 16) + (blue << 8) + alpha;
     }
     
-    // Red of UIColor (get-only)
+    /// Red of UIColor (get-only)
     public var m_red: CGFloat {
         var red: CGFloat = 0
         getRed(&red, green: nil, blue: nil, alpha: nil)
         return red
     }
     
-    // Green of UIColor (get-only)
+    /// Green of UIColor (get-only)
     public var m_green: CGFloat {
         var green: CGFloat = 0
         getRed(nil, green: &green, blue: nil, alpha: nil)
         return green
     }
     
-    // blue of UIColor (get-only)
+    /// blue of UIColor (get-only)
     public var m_blue: CGFloat {
         var blue: CGFloat = 0
         getRed(nil, green: nil, blue: &blue, alpha: nil)
         return blue
     }
     
-    // Alpha of UIColor (get-only)
+    /// Alpha of UIColor (get-only)
     public var m_alpha: CGFloat {
         // return CGColorGetAlpha(self.CGColor)
         var alpha: CGFloat = 0
