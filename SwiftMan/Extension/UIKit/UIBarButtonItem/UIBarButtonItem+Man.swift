a63c7145725afe222e4429d10b840ea82f3f9cc2
easyui <gzhuyangjun@gmail.com>
2017-06-21 20:28:10 +0800

    add UIBarButtonItem+Man.swift

---
+++ SwiftMan/Extension/UIKit/UIBarButtonItem/UIBarButtonItem+Man.swift
@@ -0,0 +1,27 @@
+//
+//  UIBarButtonItem+Man.swift
+//  SwiftMan
+//
+//  Created by IQIYI on 2017/6/21.
+//  Copyright © 2017年 cactus. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS)
+import UIKit
+
+
+// MARK: - Methods
+public extension UIBarButtonItem {
+    
+    /// SwiftMan: Add Target to UIBarButtonItem
+    ///
+    /// - Parameters:
+    ///   - target: target.
+    ///   - action: selector to run when button is tapped.
+    public func addTargetForAction(_ target: AnyObject, action: Selector) {
+        self.target = target
+        self.action = action
+    }
+    
+}
+#endif
