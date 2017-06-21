703dda12865bf950440ea6b512a9baa485a61739
easyui <gzhuyangjun@gmail.com>
2017-06-21 22:36:32 +0800

    update UILable+Man

--- SwiftMan/Extension/UIKit/UILable/UILable+Man.swift
+++ SwiftMan/Extension/UIKit/UILable/UILable+Man.swift
@@ -7,5 +7,24 @@
 //
 
 
+
+#if os(iOS) || os(tvOS)
 import UIKit
 
+
+// MARK: - Methods
+public extension UILabel {
+    
+    /// SwiftMan: Required height for a label
+    public var m_requiredHeight: CGFloat {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: frame.width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        label.attributedText = attributedText
+        label.sizeToFit()
+        return label.frame.height
+    }
+}
+#endif
